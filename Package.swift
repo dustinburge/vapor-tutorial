4e1d1777e97d8beaa8ddd5aafb56e62c1bd2830d
Dustin Burge <dburge506@gmail.com>
2018-09-08 14:54:29 -0400

    Simple website.

--- Package.swift
+++ Package.swift
@@ -4,11 +4,12 @@ import PackageDescription
 let package = Package(
     name: "TILApp",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.8"),
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.1")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor", "Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
